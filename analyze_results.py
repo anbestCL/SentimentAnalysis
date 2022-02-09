6074ee25556e85a44a45bcfc42a04a049a95b097
anbestCL <anna.bettina.steinberg@gmail.com>
2022-02-09 17:30:46 +0100

    analysis for tweets added

--- analyze_results.py
+++ analyze_results.py
@@ -1,41 +1,90 @@
 from typing import List, Tuple, Dict
 
 import pickle
+import argparse
+import os
+
 from sklearn.metrics import precision_recall_fscore_support
 
+RESULTS_DIR = "processed_data/"
+IMDB_DIR = "aclImdb/"
+TWITTER_DIR = "twitter/"
 
-def convert_label2index(label: str) -> int:
+
+def imdb_label2index(label: str) -> int:
     return 1 if label == "pos" else 0
 
 
-def get_labels(complete_dict: Dict) -> Tuple(List[int]):
+def twitter_index_remap(index: int) -> int:
+    return 1 if index == 4 else index
+
+
+def pred_label2index(label: str) -> int:
+    if label == "POSITIVE":
+        return 1
+    elif label == "NEGATIVE":
+        return 0
+    elif label == "NEUTRAL":
+        return 2
+    else:
+        return None
+
+
+def get_labels(complete_dict: Dict, datatype: str) -> Tuple[List[int]]:
     true_labels = []
     pred_labels = []
     for _, (_, true_label, result) in complete_dict.items():
-        if result["Sentiment"] == "NEUTRAL":
+        pred_label = pred_label2index(result["Sentiment"])
+
+        if pred_label is None:
             continue
 
-        elif result["Sentiment"] == "POSITIVE":
-            true_labels.append(convert_label2index(true_label))
-            pred_labels.append(convert_label2index("pos"))
+        if datatype == "imdb":
+            # imdb has only pos and neg reviews
+            if pred_label == 2:
+                continue
+            else:
+                pred_labels.append(pred_label)
+                true_labels.append(imdb_label2index(true_label))
 
-        elif result["Sentiment"] == "NEGATIVE":
-            true_labels.append(convert_label2index(true_label))
-            pred_labels.append(convert_label2index("neg"))
+        elif datatype == "twitter":
+            pred_labels.append(pred_label)
+            true_labels.append(twitter_index_remap(true_label))
+
     return (true_labels, pred_labels)
 
 
-def compute_metrics(labels: Tuple(List[int])) -> None:
+def compute_metrics(labels: Tuple[List[int]]) -> None:
     true_labels, pred_labels = labels
     score = precision_recall_fscore_support(true_labels,
                                             pred_labels,
-                                            average="macro"
+                                            average=None
                                             )
     print(f"Precision:{score[0]} \nRecall:{score[1]} \nFscore: {score[2]}")
 
 
 if __name__ == "__main__":
-    complete_dict = pickle.load(open("orig_w_results.p", "rb"))
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_type",
+        type=str,
+        required=True,
+        help="specifying data set, imdb or twitter",
+    )
+    args = parser.parse_args()
 
-    labels = get_labels(complete_dict)
+    if args.data_type == "imdb":
+        results = [filename for filename in
+                   os.listdir(f"{RESULTS_DIR}{IMDB_DIR}")
+                   if filename.startswith("orig_w")]
+        path = f"{RESULTS_DIR}{IMDB_DIR}{results[0]}"
+    elif args.data_type == "twitter":
+        results = [filename for filename in
+                   os.listdir(f"{RESULTS_DIR}{TWITTER_DIR}")
+                   if filename.startswith("orig_w")]
+        path = f"{RESULTS_DIR}{TWITTER_DIR}{results[0]}"
+    complete_dict = pickle.load(open(path, "rb"))
+
+    labels = get_labels(complete_dict, args.data_type)
+    print(f"Results for {args.data_type}")
     compute_metrics(labels)
