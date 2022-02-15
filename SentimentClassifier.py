71e3cde379afc19864347315701f96bd9e359123
anbestCL <anna.bettina.steinberg@gmail.com>
2022-02-15 18:11:13 +0100

    results dict saved to be passed on

--- SentimentClassifier.py
+++ SentimentClassifier.py
@@ -10,10 +10,11 @@ from DataLoader import DataLoader, DATA_DIR, TWITTER_DIR, AIRLINE_FILE
 
 class SentimentClassifier:
 
-    def __init__(self, data: List[Tuple]):
+    def __init__(self, data: List[Tuple], data_name: str):
         restructured = list(zip(*data))
         self._data = restructured[0]
         self._labels = restructured[2] if len(restructured) == 3 else 1
+        self._data_name = data_name
 
     def _batch(self, examples: List[str],
                batch_size: int) -> Iterator[List[str]]:
@@ -41,15 +42,19 @@ class SentimentClassifier:
         result = send_single_request(comprehend, text)
         print(result["Sentiment"])
 
-    def _save(self) -> None:
+    def _create_result_dict(self):
         complete_dict = {}
         for index, (text, label, result_dict) in enumerate(
             zip(self._data, self._labels, self.results)
         ):
             complete_dict[index] = [text, label, result_dict]
+        return complete_dict
 
-        results_file_name = "processed_data/comprehend_results.p"
-        pickle.dump(complete_dict, open(results_file_name, "wb"))
+    def _save(self) -> None:
+        self.result_dict = self._create_result_dict()
+        results_file_name = f"processed_data/comprehend_results_\
+                            {self._data_name}.p"
+        pickle.dump(self.result_dict, open(results_file_name, "wb"))
 
 
 if __name__ == "__main__":
@@ -58,6 +63,6 @@ if __name__ == "__main__":
     airline_path = f"{DATA_DIR}{TWITTER_DIR}{AIRLINE_FILE}"
     airline_data = DataLoader("airline", airline_path)
 
-    classifier = SentimentClassifier(airline_data.data)
+    classifier = SentimentClassifier(airline_data.data, "airline")
     example = "I hate this so much, but I cannot stop."
     SentimentClassifier.classify_single_text(example)
